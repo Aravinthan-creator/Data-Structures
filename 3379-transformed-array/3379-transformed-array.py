1b1d546e0e7aa04ccd3e23093135730ffe92c0ab
Aravinthan S <aravinthanwork@gmail.com>
2026-02-05 11:52:15 +0530

    Time: 57 ms (69.06%), Space: 19.4 MB (34.53%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -1,4 +1,3 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
         return [nums[(i + v) % len(nums)] for i, v in enumerate(nums)]
